063100e27059f37edd9ea4a0af9b1fca546a5639
Семен <semen_s84@mail.ru>
2025-02-04 07:08:10 +0500

    Update ddl_trig_001.sql

--- pl_sq/ddl_trig_001.sql
+++ pl_sq/ddl_trig_001.sql
@@ -1,9 +1,9 @@
-CREATE OR REPLACE TRIGGER UBRR_SRC.tr_database_src_aud
+CREATE OR REPLACE TRIGGER SRC.tr_database_src_aud
  AFTER
   DDL
  ON DATABASE
 begin
-  ubrr_src_aud_tools.Write_Aud;
+  src_aud_tools.Write_Aud;
 exception
     when others then
         null;
